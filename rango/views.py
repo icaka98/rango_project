890309cdaa2d6814028f8b631c042a37f1ab902e
icaka98 <minkov.h@gmail.com>
2018-08-11 00:37:31 +0300

    Added template

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    context = {
+        'boldmessage': 'Ooops, a template!'
+    }
+
+    return render(request, 'rango/index.html', context=context)
+
+
+def about(request):
+    return HttpResponse('About page <a href=\'/rango/\'>Go to Main page</a>')
