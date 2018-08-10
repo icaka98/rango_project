56ae0ad2bb2862be3c07ae8760f96c18c67de11e
icaka98 <minkov.h@gmail.com>
2018-08-10 23:53:07 +0300

    Initial commit

---
+++ rango_project/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+
+from rango import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^rango/', include('rango.urls'))
+]
