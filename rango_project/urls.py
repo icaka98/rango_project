62c6e7a3515a493eb4fb4e9c44fc1234c91f56e2
icaka98 <minkov.h@gmail.com>
2018-08-17 02:37:39 +0300

    Finishing with Redux implementing

--- rango_project/urls.py
+++ rango_project/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
+from registration.backends.simple.views import RegistrationView
 
 from rango import views
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user=None):
+        return '/rango/'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^$', views.index, name='index'),
     re_path(r'^rango/', include('rango.urls')),
-    re_path(r'accounts/', include('registration.backends.simple.urls'))
+    re_path(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    re_path(r'^accounts/', include('registration.backends.simple.urls'))
 ]
